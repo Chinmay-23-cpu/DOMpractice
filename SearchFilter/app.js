cb3ea355f685226e62d66bd5fdc36c5ae57e3025
Chinmay <bhatchinmay172@gmail.com>
2026-08-02 12:26:18 +0530

    filterList

---
+++ SearchFilter/app.js
@@ -0,0 +1,21 @@
+function SearchFilter()
+{
+    const inputBox = document.getElementById('SearchIt');
+    const UserValue = inputBox.value;
+    const GivenList = document.querySelectorAll('#TheList li')
+
+    GivenList.forEach((item)=>{
+        let text = item.innerText;
+         text = text.toLowerCase();
+        let inputVal = UserValue.toLowerCase();
+        let found = text.includes(inputVal)
+         if(found)
+         {
+            item.style.display = 'block'
+         }
+         else
+         {
+            item.style.display = 'none'
+         }
+    })
+}
