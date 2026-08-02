97cf8ba1d0efb9b62d57d4bfb0f4153ed36e771a
Chinmay <bhatchinmay172@gmail.com>
2026-08-02 16:18:16 +0530

    inserting an element 1

--- basicManipulationDOM/app.js
+++ basicManipulationDOM/app.js
@@ -4,3 +4,10 @@ const ParagraphElem = document.createElement('p')
 ParagraphElem.innerText = "hey this line is dynamically created"
 document.body.appendChild(ParagraphElem)
 // console.log(ParagraphElem)
+
+/*Inserting an element*/
+
+const SpanEle = document.createElement('span')
+SpanEle.innerText = "this is span inserted"
+const parelem = document.querySelector('p')
+document.body.insertBefore(SpanEle,parelem)
