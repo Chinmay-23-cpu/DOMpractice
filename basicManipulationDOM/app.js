05e711b20b6f2bbc808d956ec755e6770cd43bc9
Chinmay <bhatchinmay172@gmail.com>
2026-08-02 16:01:24 +0530

    dynamic creation of a paragraph element

---
+++ basicManipulationDOM/app.js
@@ -0,0 +1,6 @@
+/*Creating an element */
+
+const ParagraphElem = document.createElement('p')
+ParagraphElem.innerText = "hey this line is dynamically created"
+document.body.appendChild(ParagraphElem)
+// console.log(ParagraphElem)
